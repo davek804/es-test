aceefd35bf18863418834d05c7a73152c00668a2
David Kelley <dakelley@akamai.com>
2017-05-07 22:49:22 -0400

    Add all needed files. Second to last check in before sending documents tomorrow morning.

--- README.md
+++ README.md
@@ -1,12 +1,31 @@
 # es-test
-Repository for SALS quiz
+Repository for SALS 
 
-This repo will es launch an AWS VM running a secure ElasticSearch service. 
+This repo will es launch an AWS VM running an Elasticsearch service secured with a unique username and password. 
 
 ** Dependencies **
-- Use Mac / Linux for ease. 
+- Use Mac / Linux for ease and confidence the script need not be customized for other machines. 
 - You have installed pip. Else, follow https://pip.pypa.io//en/latest/installing/ (You will probably need root privs.)
-- You have installed AWS CLI. Else, follow https://aws.amazon.com/cli/ (I suggest something like `sudo -H pip install awscli --upgrade --ignore-installed six` for Mac.)
-- You have properly configured AWS CLI using `aws configure`. 
+- You have installed AWS CLI. Else, follow https://aws.amazon.com/cli/ (I suggest something like `sudo -H pip install awscli --upgrade --ignore-installed six` for Mac specifically.)
+- You have properly configured AWS CLI using `aws configure` for your AWS account. 
+- You have 0 EC2 instances prior to running this script. 
+- You have only the default security group prior to running this script. 
 
-Just run init.sh. 
+** Instructions **
+1) Follow and complete all of the DEPENDENCIES above.  
+2) Run `init.sh`. 
+3) Confirm the final output of the `init.sh` script is an echo that states `Elasticsearch installed, running, and secured with a non-default password.`. Above that, you should see the JSON response from a successfully queried Elasticsearch instance, using a non-default password. 
+
+** Conversation Street ** 
+My goal for this script was something along these lines: 
+1) Brings up an AWS instance
+2) Installs ElasticSearch configured in a way that requires credentials and provides encrypted communication
+3) Demonstrates that it is functioning
+
+I am confident I've completed #1 and #3. However, I've completed all of #3 except for the full encrypting. I've successfully encrypted the Elasticsearch instance and instructed it to run within SSL paramaters - but I've not succeeded in being able to securely communicate with it via HTTPS curls (using '--cacert'. That'd tag more digging and reading than I am prepared to explore for a pureply exploratory project (but how much fun it's been trying to debug it!)).
+
+I find it very enjoyable to complete this whole project with a single bash script. Of course, this could have been accomplished with Python, Ruby, Chef, AWS' solution - anything. What I enjoyed, howver, was making something reasonably dynamic and error/configuration proof that felt 'stable'. Of course, dozens of scenarios could have caused problems. However, I ran the script dozens of times to come up with the requisite conditions I needed to test against to make the script fairly encapsulated and reusuable. Of course, YMMV.
+
+In terms of resources I consulted in order to get the job done, I think the answer can be summed up with two domains: stackoverflow.com; aw.amazon.com. Of course, a few Bash references, AWS CLI third-party threads, and old-school 2004 V-bulletin forums also played supporting roles. Seriously, I'd share my play-by-play browser history except, come on now. 
+
+I thought this was a fun exercise. I finished at exactly four hours (bearing in mind I was multitasking during the first crack at it) with a script that got everything done except for an SSL conncection. I knew going in to it that the SSL aspect would be the biggest hurdle for me. I spent a decent amount of time on Saturday working on fixing up the SSL, til I determined I'd not made any progress. So I broke things apart and made it all more 'secured' in the sense that I tested for conditions and made the script file itself more comfortable and testable. I knew I'd need to do this, from the moment I started writing. Anyways. After all that, I came back to the SSL problem. Amazingly, I got Elasticsearch up and running with SSL. Turns out I had some path/FQDN issues prior. It was great to see it running under HTTPS. Unfortunately, things were broken when trying to properly `curl` with `HTTPS`. I think with some more time googling which certificate I needed to use, and how, I could have gotten it up and running. But hey, let's discuss later. After a few more hours working on it on Saturday total, I put the project down. I cleaned up the SH file, and updated the zip late on Sunday, prior to commit and push to my repo. After that, I sent it back to your team. Enjoy!
--- init.sh
+++ init.sh
@@ -1,75 +1,160 @@
 #!/bin/bash
-# Set up some initial variables we will use at various times: 
-securityGroup=esearch-sg
+
+# Establish the bae variable's we'll need. 
+
+SECURITY_GROUP=esearch-sg
+SECURITY_GROUP_ID=sg-7bf86100
 AMI=ami-4836a428
-instance=i-0338d7b3fa3c53e2c
-ipaddress=35.165.160.179
+INSTANCE=i-05aee954fa26c6bb0
+EC2_IP=35.165.153.109
+OUR_IP=$(curl -s ipinfo.io/ip)
+EC2_KEY=elasticsearch-key.pem
+# That'll do. 
 
-if [[ $# -ne 0 ]]; then 
-	# Just a basic check to confirm an argument exists. 
-	if [[ "$1" = "initialize" ]]; then 
-		echo "The arguement is to initialize! "
-		echo "Initialize a security group for our new EC2 instance:"
-		#aws ec2 create-security-group --group-name "$securityGroup" --description "security group for elastic search environment in EC2" --dry-run 
+# Introduce the user that doesn't read the code to what we're going to do and use. 
+echo "Welcome! We are going to build Elasticsearch from scratch. "
+sleep 1
 
-		echo "Let's allow traffic over port 22:"
-		#aws ec2 authorize-security-group-ingress --group-name "$securityGroup" --protocol tcp --port 9200 --cidr 23.79.236.14/0
+echo "Here's what we know so far: "
+echo "Security Group..................................................: $SECURITY_GROUP"
+echo "Security Group ID...............................................: $SECURITY_GROUP_ID"
+echo "AMI.............................................................: $AMI"
+echo "Instance........................................................: $INSTANCE"
+echo "EC2 IP Address..................................................: $EC2_IP"
+echo "Our Machine IP..................................................: $OUR_IP"
+echo "Our EC2 Key.....................................................: $EC2_KEY"
+sleep 2
 
-		echo "Create a key-pair for access: "
-		#aws ec2 create-key-pair --key-name elasticsearch-key --query 'KeyMaterial' --output text > elasticsearch-key.pem
-		# todo - clean up permissions
+echo " "
+echo "Confirm if $SECURITY_GROUP exists as the first Security Group in your AWS instance."
 
-		echo "Create an EC2 instance:"
-		# TODO - send the resulting string to the $instance variable above. 
-		#aws ec2 run-instances --image-id "$AMI" --security-group-ids sg-7bf86100 --count 1 --instance-type t2.micro --key-name elasticsearch-key --query 'Instances[0].InstanceId'
+if [[ $(aws ec2 describe-security-groups --group-names esearch-sg --query SecurityGroups[0].GroupName | sed 's/\[//' | sed -e 's/\"//g') -eq "$securityGroup" ]]; then 
+	echo "It does! Nothing to be done, then."
+	echo " "
+else 
+	echo "They do not match, so let's make the group."
+	aws ec2 create-security-group --group-name "$securityGroup" --description "security group for elastic search environment in EC2" 
+	# Make sure we have the security group ID stored for later, when we need to make the instance. 
+	SECURITY_GROUP_ID=$(aws ec2 describe-security-groups --group-names esearch-sg --query SecurityGroups[0].GroupId | sed 's/\"//g')
+	echo "We will also need to allow incoming traffic, taking care of that. "
+	aws ec2 authorize-security-group-ingress --group-name "$SECURITY_GROUP" --protocol tcp --port "9200" --cidr "$OUR_IP"/32
+	aws ec2 authorize-security-group-ingress --group-name "$SECURITY_GROUP" --protocol tcp --port "22" --cidr "$OUR_IP"/32
+	aws ec2 authorize-security-group-egress --group-id "$SECURITY_GROUP_ID " --ip-permissions '[{"IpProtocol": "tcp", "FromPort": 0, "ToPort": 65535, "IpRanges": [{"CidrIp": "0.0.0.0/0"}]}]'
+	echo " " 
+fi 
 
-		echo "Let's fetch the public IP address of our instance:"
-		# TODO - send the resulting string to the $ipaddress variable above. 
-		#aws ec2 describe-instances --instance-ids "$instance" --query 'Reservations[0].Instances[0].PublicIpAddress'
-
-		echo "Blow away Java 1.7 and install 1.8, lol. Did I expect to not have to do this.... naive?"
-		#ssh -i elasticsearch-key.pem ec2-user@"$ipaddress" 'sudo yum install java-1.8.0; sudo yum remove java-1.7.0-openjdk'
-
-		echo "Add some swap space to deal with elastic search wanting more memory"
-		#ssh -i elasticsearch-key.pem ec2-user@"$ipaddress" 'sudo dd if=/dev/zero of=/swapfile bs=1M count=5000;sudo mkswap /swapfile; sudo swapon /swapfile' \\
-
-		echo "Download, unzip ElasticSearch:"
-		ssh -i elasticsearch-key.pem ec2-user@"$ipaddress" 'curl -L -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.4.0.tar.gz; tar -xvf elasticsearch-5.4.0.tar.gz'
-
-		echo "Install X-Pack, so we can start securing things: "
-		ssh -i elasticsearch-key.pem ec2-user@"$ipaddress" 'cd elasticsearch-5.4.0/bin; ./elasticsearch-plugin install x-pack'
-
-		echo "Let's launch ElasticSearch as a daemon so we don't need to worry about relaunching it later, unless the box reboots: "
-		ssh -i elasticsearch-key.pem ec2-user@"$ipaddress" 'cd elasticsearch-5.4.0/bin; ./elasticsearch  -Ecluster.name=my_cluster -Enode.name=my_node_name -d'
-		echo "Sleeping for 20 seconds to let things get settled. "
-		sleep 20
-	fi
+echo "Confirming if we have keys properly established for our EC2 instance. "
+if [[ -f "$EC2_KEY" ]]; then 
+	#echo $(cat "$EC2_KEY")
+	echo "Yes, we have an EC2 key stored as $EC2_KEY"
+	echo " "
+else 
+	aws ec2 create-key-pair --key-name elasticsearch-key --query 'KeyMaterial' --output text > elasticsearch-key.pem
+	echo " "
 fi
 
-#ssh -i elasticsearch-key.pem ec2-user@35.165.160.179
+echo "Next, let's see if we have an EC2 instance that matches - expecting: $INSTANCE"
+# Things became a mess where Amazon values became octals in bash. Ugh. 
+AWS_INSTANCE=$(aws ec2 describe-instances --query Reservations[].Instances[].InstanceId | sed -e 's/\[//' -e 's/\]//' -e 's/\"//g' -e '/^\s*$/d' -e 's/^    //')		
+if [[ "$AWS_INSTANCE" == "$INSTANCE" ]]; then 
+	echo "Confirmed, you have an instance with the correct Instance ID. "
+	echo " " 
+else 
+	echo "Looks like we still need to make the instance, let's do it. "
+	INSTANCE=$(aws ec2 run-instances --image-id "$AMI" --security-group-ids "$SECURITY_GROUP_ID" --count 1 --instance-type t2.micro --key-name elasticsearch-key --query 'Instances[0].InstanceId' | sed 's/\"//g')
+	# Store the instance value. 
+	echo "$INSTANCE"
+	# Make sure the script is updated to the new value. 
+	sed -i.bak "s/^INSTANCE=.*$/INSTANCE=$INSTANCE/" init.sh
+	rm -rf init.sh.bak
+	echo " "
+fi  
 
+echo "Fetching the public IP address of our instance and storing it."
+IP=$(aws ec2 describe-instances --query 'Reservations[].Instances[].PublicIpAddress' | sed -e 's/\[//' -e 's/\]//' -e 's/\"//g' -e '/^\s*$/d' -e 's/^    //')
+# Make sure the script is updated to the new value. 
+sed -i.bak "s/^EC2_IP=.*$/EC2_IP=$IP/" init.sh
+rm -rf init.sh.bak
+echo "$EC2_IP"
+echo " "
+sleep 1
 
+echo "Now we have everything needed to connect to the instance and install ElasticSearch!"
+echo " "
+sleep 1
 
+echo "We'll use rsync instead of SCP to only copy the ElasticSearch zip if it's needed. "
+rsync -a --ignore-existing -rave "ssh -i $EC2_KEY" es.zip ec2-user@$EC2_IP:~/
+echo " "
+sleep 1
 
-ssh -i elasticsearch-key.pem ec2-user@"$ipaddress" /bin/bash << EOF
+echo "Unfortunately our EC2 probably doesn't have the required version of Java. This has literally never ever happened before."
+echo "Let's install Java 1.8 and remove 1.7"
+ssh -i $EC2_KEY ec2-user@$EC2_IP /bin/bash << EOF
+	sudo yum -y install java-1.8.0
+	sudo yum -y remove java-1.7.0-openjdk
+EOF
+echo " " 
+sleep 1
+
+echo "Add some swap space to deal with elastic search wanting more memory"
+echo "TODO! Make this dynamic and only happen once."
+ssh -i elasticsearch-key.pem ec2-user@"$ipaddress" 'sudo dd if=/dev/zero of=/swapfile bs=1M count=3800;sudo mkswap /swapfile; sudo swapon /swapfile' 
+echo " "
+sleep 1
+
+echo "Unzip ElasticSearch:"
+ssh -i $EC2_KEY ec2-user@$EC2_IP 'unzip -n es.zip'
+echo " "
+sleep 1
+
+echo "Install X-Pack, so we can start securing things: "
+ssh -i $EC2_KEY ec2-user@$EC2_IP 'cd es/bin; ./elasticsearch-plugin -s install x-pack'
+echo " "
+sleep 1
+
+echo "Copying certificate blueprint to our server if needed."
+rsync -a --ignore-existing -rave "ssh -i $EC2_KEY" instances.yml ec2-user@$EC2_IP:~/
+
+# Last round quick tinkering to get certificates & SSL up and running. 
+# es/bin/x-pack/certgen -in ~/instances.yml
+# unzip /home/ec2-user/es/config/x-pack/certificate-bundle.zip
+# echo "xpack.ssl.key: /home/es/config/x-pack/my_node_name.key" >> /home/ec2-user/es/config/elasticsearch.yml
+# echo "xpack.ssl.certificate: /home/es/config/x-pack/my_node_name.crt" >> /home/ec2-user/es/config/elasticsearch.yml
+# echo "xpack.ssl.certificate_authorities: [ '/home/es/config/x-pack/ca.crt' ]" >> /home/ec2-user/es/config/elasticsearch.yml
+# echo "xpack.security.transport.ssl.enabled: true" >> /home/ec2-user/es/config/elasticsearch.yml
+# echo "xpack.security.http.ssl.enabled: true" >> /home/ec2-user/es/config/elasticsearch.yml
+
+echo "Let's launch ElasticSearch so we don't need to worry about relaunching it later, unless the box reboots (launch as daemon): "
+cd es/bin; ./elasticsearch  -Ecluster.name=my_cluster -Enode.name=my_node_name -d
+echo "Sleeping for 20 seconds to let things get settled. "
+sleep 20
+
+ssh -i $EC2_KEY ec2-user@$EC2_IP /bin/bash << EOF
 echo "Let's prove that we NEED to use authentication, by requesting without any credentials: "
-curl -XGET 'http://localhost:9200/'
-sleep 1
+echo " "
+curl -s -XGET 'http://localhost:9200/'
+echo " "
+sleep 2
 
-echo
 echo "Hmm, that didn't look too good, did it? Let's use the default un/pw and see the node is responsive: "
-curl -XGET 'http://localhost:9200/' -u elastic:changeme
-sleep 1
+echo " "
+curl -s -XGET 'http://localhost:9200/' -u elastic:changeme
+sleep 2
+echo " "
 
-echo
 echo "Great. Let's just update the password to something a _little_ more secure (we'll use elasticpassword): "
+echo " "
 curl -POST -u elastic:changeme 'localhost:9200/_xpack/security/user/elastic/_password' -H "Content-Type: application/json" -d '{
   "password" : "elasticpassword"
 }'
-sleep 1
+sleep 2
+echo " "
 
-echo
 echo "Alright! Let's use the new password now, to confirm everything: " 
+echo " "
 curl -XGET 'http://localhost:9200/' -u elastic:elasticpassword
+sleep 2
+echo " "
+echo "ELASTICSEARCH INSTALLED, RUNNING, AND SECURED WITH A NON-DEFAULT PASSWORD/"
 EOF
-
